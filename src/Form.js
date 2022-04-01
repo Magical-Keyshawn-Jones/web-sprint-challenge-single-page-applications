92782cb1a19810fd87e2b920bae09a16995bd5c9
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-04-01 14:51:54 -0500

    finished everything

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import { Route, Link } from 'react-router-dom';
 import { useState } from 'react';
-import formTest from './Validation';
+import axios from 'axios';
 import * as yup from 'yup';
 import React from 'react'
 
@@ -8,27 +8,48 @@ function Form () {
 
     // Initial Values
     const initialFormValues = {
-        name: ''
+        name: '',
+        specialText: '',
+        pepperoni: false,
+        olives: false,
+        cheese: false,
+        pineapples: false
     }
     
     // Error Values
     const errorValues = {
-        name: ''
+        name: '',
+        specialText:''
     }
 
     // Storing Values
     const [formValues, setFormValues] = useState(initialFormValues);
-    const [error, setError] = useState(errorValues);
+    const [errors, setError] = useState(errorValues);
+
+    // console.log(formValues)
+
+    // Making Test form formValues
+    const formTest = yup.object().shape({
+        name: yup
+        .string()
+        .min(2,'name must be at least 2 characters'),
+        specialText: yup.string(),
+        pepperoni: yup.boolean(),
+        olives: yup.boolean(),
+        cheese: yup.boolean(),
+        pineapples: yup.boolean()
+    })
+    
 
     // Adding Input Test
     function validator (name, value) {
         yup.reach(formTest, name)
         .validate(value)
         .then(()=>{
-            setError({...error, [name]: ''})
+            setError({...formValues, [name]: ''})
         })
         .catch((err)=>{
-            setError({...error, [name]: err.error[0]})
+            setError({...formValues, [name]: err.errors[0]})
         })
     }
 
@@ -37,9 +58,38 @@ function Form () {
         const { name, value, id, checked, type } = event.target
 
         const valueToUse = type === 'checkbox' ? checked : value
-        validator(name,valueToUse)
 
-        setFormValues({...formValues, [name]: value})
+        setFormValues({...formValues, [name]: valueToUse})
+
+        validator(name,valueToUse)
+    }
+
+    // Making a Post sender for Form 
+    function sender (letter) {
+        axios.post('https://reqres.in/api/orders', letter)
+        .then(object => console.log(object))
+        .catch(err => console.log('error!',err))
+    }
+
+    function cleanPosting () {
+
+        const refinement = {
+            name: formValues.name.replace(/\s+/g, ' ').trim(),
+            specialText: formValues.specialText.replace(/\s+/g, ' ').trim(),
+            toppings: ['pepperoni', 'olives', 'cheese', 'pineapples'].filter(toppers => formValues[toppers])
+        }
+
+        sender(refinement)
+    }
+
+    // Making Submit function for onSubmit
+    function onSubmit  (event) {
+
+        event.preventDefault()
+
+        cleanPosting()
+
+        setFormValues(initialFormValues)
     }
     
 
@@ -53,17 +103,67 @@ function Form () {
             I'm about the pizza
             <Route path='/pizza'>
                 Hi I'm a form
-                <form id='pizza-form'>
-                    <input
-                    type='text'
-                    id='name-input'
-                    name='name'
-                    value={formValues.name}
-                    onChange={onChange}
-                    />
+                <form id='pizza-form' onSubmit={onSubmit}>
+                    <label>
+                        Name of Order
+                        <input
+                        type='text'
+                        id='name-input'
+                        name='name'
+                        value={formValues.name}
+                        onChange={onChange}
+                        />
+                    </label>
                     <div>
-                        {error.name}
+                        {errors.name}
                     </div>
+                    <label>
+                        Pizza Size
+                        <select id='size-dropdown'>
+                            <option>Only one size! sorry! 30"</option>
+                            <option>32 size please</option>
+                        </select>
+                    </label>
+                    <label>
+                        Toppings
+                        <input
+                            type='checkbox'
+                            name='pepperoni'
+                            checked={formValues.pepperoni}
+                            onChange={onChange}
+                        />
+                        <input
+                            type='checkbox'
+                            name='olives'
+                            checked={formValues.olives}
+                            onChange={onChange}
+                        />
+                        <input
+                            type='checkbox'
+                            name='cheese'
+                            checked={formValues.cheese}
+                            onChange={onChange}
+                        />
+                        <input
+                            type='checkbox'
+                            name='cheese'
+                            checked={formValues.cheese}
+                            onChange={onChange}
+                        />
+                    </label>
+                    <br/>
+                    <label>
+                        Special Instructions
+                        <input
+                            id='special-text'
+                            type='text'
+                            name='specialText'
+                            value={formValues.specialText}
+                            onChange={onChange}
+                        />
+                    </label>
+                    <br/>
+                    <button id='order-button'>Order</button>
                 </form>
             </Route>
         </div>
--- src/Validation.js
+++ src/Validation.js
@@ -1,10 +1,10 @@
-import * as yup from 'yup';
+// import * as yup from 'yup';
 
-const formTest = yup.object().shape({
-    name: yup
-    .string()
-    .required()
-    .min(2,'name must be at least 2 characters')
-})
+// const formTest = yup.object().shape({
+//     name: yup
+//     .string()
+//     .min(2,'name must be at least 2 characters')
+//     .required('name must be at least 2 characters')
+// })
 
-export default formTest;
+// export default formTest;
